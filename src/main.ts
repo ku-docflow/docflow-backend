a34e4b13076fe7b81a486908a735e08eae810239
Ji Hwan An <angeljihwan@gmail.com>
2025-04-07 17:09:22 +0900

    Port 4000

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ dotenv.config({ path: './src/.env' });
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
