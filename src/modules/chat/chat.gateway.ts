22eb1f2f60a3b10acbd94bc91b12eb5c2688ca57
Ji Hwan An <angeljihwan@gmail.com>
2025-04-11 16:01:03 +0900

    Websocket FK Error

--- src/modules/chat/chat.gateway.ts
+++ src/modules/chat/chat.gateway.ts
@@ -2,11 +2,12 @@ import {
   WebSocketGateway,
   SubscribeMessage,
   MessageBody,
-  ConnectedSocket,
   WebSocketServer,
+  WsException,
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 import { ChatService } from './chat.service';
+import { QueryFailedError } from 'typeorm';
 
 @WebSocketGateway({ cors: true })
 export class ChatGateway {
@@ -18,22 +19,31 @@ export class ChatGateway {
   @SubscribeMessage('send_message')
   async handleSendMessage(
     @MessageBody()
-    payload: { chatroom_id: number; sender_id: string; text: string },
-    @ConnectedSocket() _client: Socket,
+    payload: {
+      chatroom_id: number;
+      sender_id: string;
+      text: string;
+    },
   ) {
-    const saved = await this.chatService.saveMessage(
-      payload.chatroom_id,
-      payload.sender_id,
-      payload.text,
-    );
+    try {
+      const saved = await this.chatService.saveMessage(
+        payload.chatroom_id,
+        payload.sender_id,
+        payload.text,
+      );
 
-    this.server.to(`room-${payload.chatroom_id}`).emit('receive_message', {
-      id: saved.id,
-      chatroom_id: saved.chatroom_id,
-      sender_id: saved.sender_id,
-      text: saved.text,
-      timestamp: saved.timestamp,
-    });
+      this.server.to(`room-${payload.chatroom_id}`).emit('receive_message', {
+        id: saved.id,
+        chatroom_id: saved.chatroom_id,
+        sender_id: saved.sender_id,
+        text: saved.text,
+        timestamp: saved.timestamp,
+      });
+    } catch (err) {
+      if (err instanceof QueryFailedError && (err as any).code === '23503') {
+        throw new WsException('Chatroom or User does not exist');
+      }
+    }
   }
 
   handleConnection(client: Socket) {
