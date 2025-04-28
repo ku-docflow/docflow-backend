009b6e44b4c960cb6358343f563543dc37f59fcd
Ji Hwan An <angeljihwan@gmail.com>
2025-04-28 17:11:46 +0900

    chore: add team_id to document entity

--- src/modules/document/document.entity.ts
+++ src/modules/document/document.entity.ts
@@ -1,9 +1,14 @@
-import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, ManyToOne, JoinColumn } from 'typeorm';
+import { Team } from '../team/team.entity';
 
 @Entity('document')
 export class Document {
 	@PrimaryGeneratedColumn('increment', { type: 'bigint' })
 	id: number;
+
+	@ManyToOne(() => Team, { onDelete: 'CASCADE' })
+	@JoinColumn({ name: 'team_id' })
+	team: Team;
 
 	@Column('text')
 	text: string;
