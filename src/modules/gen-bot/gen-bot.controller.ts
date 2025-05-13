4275dede4f3b1443693fe22f0c1f8fc5fa4319f1
hi-story4 <wnsgur1503@naver.com>
2025-05-13 14:57:12 +0900

    fix: genbot auth guard

--- src/modules/gen-bot/gen-bot.controller.ts
+++ src/modules/gen-bot/gen-bot.controller.ts
@@ -1,21 +1,22 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { GenBotService } from './gen-bot.service';
 import { GenBotRequestDto } from './dto/gen-bot.dto';
+import { FirebaseRequest } from '../../common/interfaces/firebase-request.interface';
+import { FirebaseAuthGuard } from '../../common/guards/firebase-auth.guard';
 
-// @UseGuards(FirebaseAuthGuard)
+@UseGuards(FirebaseAuthGuard)
 @Controller('gen-bot')
 export class GenBotController {
     constructor(private readonly genBotService: GenBotService) {
     }
 
-
     @Post()
     async createDocument(
-        // @Req() req: FirebaseRequest,
+        @Req() req: FirebaseRequest,
         @Body() query: GenBotRequestDto,
     ) {
         console.log(query);
 
-        return this.genBotService.createDocByBot('userId', query)
+        return this.genBotService.createDocByBot(req.user.id, query)
     }
 }
